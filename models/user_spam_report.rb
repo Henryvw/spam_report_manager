7bbfe68eb9afa5bbf6c61cc6d08e23a5ecd552c7
Henry van Wagenberg <henryvw1@gmail.com>
2019-09-03 08:59:23 +0200

    Add new tests + configs to spec
    - For Capybara requirements and library loading
    - Write new tests for buttons

--- models/user_spam_report.rb
+++ models/user_spam_report.rb
@@ -1,7 +1,6 @@
 require 'active_record'
 
-# Not sure whether I need this database connector... but leaving it in...
-DB_ENV ||= 'development'
+DB_ENV = ENV['RACK_ENV'] || 'development'
 connection_details = YAML::load(File.open('db/spam_database.yml'))
 ActiveRecord::Base.establish_connection(connection_details[DB_ENV])
 
--- spec/spam_reporter_spec.rb
+++ spec/spam_reporter_spec.rb
@@ -1,12 +1,31 @@
+ENV['RACK_ENV'] = 'test'
 require 'rspec'
+require 'capybara'
+require 'capybara/rspec'
+require 'capybara/dsl'
 require 'rack/test'
+
 require_relative '../spam_report_manager.rb'
 
+Capybara.configure do |c|
+  c.app_host = "http://localhost:4567"
+  c.default_driver = :rack_test
+end
+
+RSpec.configure do |config|
+  config.include Capybara::DSL
+end
+
 # Please Note: Given that this is a mini project and in the interest of time I did NOT create a test database. Naturally, I WOULD do that if this were a real project. I would not be saving and destroying objects and records on the actual database.
-describe SpamReportManager do
+describe SpamReportManager, type: :feature do
 
   include Rack::Test::Methods
-  
+  include Capybara
+  include Capybara::DSL
+  Capybara.current_driver = :rack_test
+  Capybara.app = SpamReportManager
+  Capybara.run_server = false
+
   def app
     SpamReportManager.new
   end
@@ -19,25 +38,29 @@ describe SpamReportManager do
     end
 
     it 'shows a list of reported messages' do
+      new_test_report = UserSpamReport.new(id: 0, state: "OPEN", spam_report_id: "000aaa", payload_report_type: "SPAM", payload_message: "UhOH")
+      new_test_report.save
+
       get '/spam_dashboard' do
-        expect(last_response.body).to include('ID', 'Type', 'Message')
-      end
+
+        expect(last_response.body).to include('ID', 'Type', 'Message') end
     end
 
     it 'shows the spam Type, ID, state and message on a spam report' do
-      new_report = UserSpamReport.new(id: 0, state: "OPEN", spam_report_id: "12345a", reference_type: "SPAM", payload_message: "Friedrich Nietschze findet das hier ist spam.")
-      new_report.save
+      new_test_report = UserSpamReport.new(id: 1, state: "OPEN", spam_report_id: "111bbb", payload_report_type: "SPAM", payload_message: "Friedrich Nietschze findet das hier ist spam.")
+      new_test_report.save
 
       get '/spam_dashboard' do
-        expect(last_response.body).to include("OPEN", "12345a", "SPAM", "Friedrich Nietschze findet das hier ist spam.")
+        expect(last_response.body).to include("OPEN", "111bbb", "SPAM", "Friedrich Nietschze findet das hier ist spam.")
       end
-
-      new_report.destroy
     end
   end
  
   context 'Block a message' do
     it 'can access a button to block a message reported as spam' do
+      get '/spam_dashboard' do
+        expect(last_response.body).to include("<button>Resolve</button>")
+      end
     end
 
     it 'can, after clicking, see that this spam-reported message will now be blocked' do
@@ -46,9 +69,19 @@ describe SpamReportManager do
 
   context 'Resolve a ticket' do
     it 'can access a button to resolve a ticket reported as spam' do
+      get '/spam_dashboard' do
+        expect(last_response.body).to include("<button>Block</button>")
+      end
     end
 
-    it 'removes it from our dashboard' do
+    it 'when clicked, removes that spam report from our dashboard' do
+      UserSpamReport.find(id=0).destroy
+      UserSpamReport.find(id=1).destroy
+      new_test_report = UserSpamReport.new(id: 2, state: "OPEN", spam_report_id: "333ccc", payload_report_type: "SPAM", payload_message: "Schiller sagt: SPAM!")
+      new_test_report.save
+      visit('/spam_dashboard')
+      find('button', text: 'Resolve').click
+      expect(page).not_to have_content '333ccc'
     end
 
     it 'makes a PUT request to endpoint /reports/:reportID' do
@@ -56,5 +89,11 @@ describe SpamReportManager do
 
     it 'updates report database to CLOSED' do
     end
+  end
+
+  #Cleanup
+  all_test_reports = UserSpamReport.all
+  all_test_reports.each do |report|
+    report.destroy
   end
 end
